f934909146cd0acff15e7691f90e0b3124bef2e6
Andrzej <kierski.andrzej@gmail.com>
2016-12-28 14:44:00 +0100

    intro section - done

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 
 //=require modules/jquery-2.2.4.min.js
-//=require modules/menu.js
 //=require modules/jquery.easing.1.3.js
+//=require modules/menu.js
+//=require modules/intro.js
 
 $(document).ready(function() {
 
   // menu init
   menu.init();
+
+  // intro section
+  introSection.init();
 
 });
